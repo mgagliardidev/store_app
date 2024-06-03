38102ff4c65247a15dedca6e7c49b8cdc121ae58
mgagliardi.dev <mgagliardi.dev@gmail.com>
2024-06-03 17:58:19 +0200

    add roboto fonts

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Roboto'),
+      home: const HomePage(),
     );
   }
 }
