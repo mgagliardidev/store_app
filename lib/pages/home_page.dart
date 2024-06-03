9417f6160aa38845bb6eee96644cf5c2d9795a48
mgagliardi.dev <mgagliardi.dev@gmail.com>
2024-06-03 17:58:31 +0200

    init appbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Home')));
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text(
+        'Hi, Matteo!', //TODO: replace with username
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      actions: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(shape: const CircleBorder()),
+          onPressed: () {},
+          child: ClipOval(
+              child: Image.network(
+                  "https://4.bp.blogspot.com/-Jx21kNqFSTU/UXemtqPhZCI/AAAAAAAAh74/BMGSzpU6F48/s1600/funny-cat-pictures-047-001.jpg",
+                  fit: BoxFit.fill,
+                  width: 50,
+                  height: 50)),
+        )
+      ],
+    ));
   }
 }
