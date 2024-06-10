2ce8569e0950282c34412850b6ec79e1f7ce46d6
mgagliardi.dev <mgagliardi.dev@gmail.com>
2024-06-10 18:22:03 +0200

    add favourite btn to prod card

--- lib/components/product_card.dart
+++ lib/components/product_card.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:store_app/api/product_service.dart';
 import 'package:store_app/models/product.dart';
@@ -24,48 +25,66 @@ class ProductCard extends StatelessWidget {
 
     return SizedBox(
       child: Card(
-        elevation: 0,
+        elevation: 3,
         shape: RoundedRectangleBorder(
           side: const BorderSide(
-              color: Color.fromARGB(179, 210, 207, 207), width: 2),
+              color: Color.fromARGB(179, 210, 207, 207), width: 1),
           borderRadius: _borderRadius,
         ),
         //color: Colors.white,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius: _borderRadius,
-                child: Image.network(
-                  ProductService().getImageUrl(product.previewImage),
-                  fit: BoxFit.fill,
-                  height: imageHeight,
-                  width: double.infinity,
-                ),
+        child: Stack(alignment: Alignment.topLeft, children: [
+          ClipRRect(
+            borderRadius: _borderRadius,
+            child: Image.network(
+              ProductService().getImageUrl(product.previewImage),
+              fit: BoxFit.fill,
+              height: imageHeight,
+              width: double.infinity,
+            ),
+          ),
+          const SizedBox(height: 4),
+          Positioned(
+            top: 190,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Row(
+                children: [
+                  RichText(
+                      overflow: TextOverflow.fade,
+                      text: TextSpan(children: [
+                        TextSpan(
+                            text: product.name,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                                fontSize: 13)),
+                        TextSpan(
+                          text:
+                              '\n${product.price.toStringAsFixed(2)}€      ${product.stars} ⭐',
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w300,
+                              color: Colors.black,
+                              fontSize: 12),
+                        )
+                      ])),
+                ],
               ),
-              const SizedBox(height: 4),
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: RichText(
-                    text: TextSpan(children: [
-                  TextSpan(
-                      text: product.name,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                          fontSize: 12)),
-                  TextSpan(
-                    text:
-                        '\n${product.price.toStringAsFixed(2)}€      ${product.stars} ⭐',
-                    style: const TextStyle(
-                        fontWeight: FontWeight.w300,
-                        color: Colors.black,
-                        fontSize: 11),
-                  )
-                ])),
-              )
-            ]),
+            ),
+          ),
+          Positioned(
+            top: 10,
+            left: 120,
+            child: IconButton.filled(
+              iconSize: 25,
+              onPressed: () {},
+              style: IconButton.styleFrom(backgroundColor: Colors.white60),
+              icon: const Icon(
+                Icons.favorite_outline_rounded,
+                color: Colors.black,
+              ),
+            ),
+          )
+        ]),
       ),
     );
   }
