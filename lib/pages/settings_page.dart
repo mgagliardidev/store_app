80f7c7c3e48401a761428d80e3708802b47235db
mgagliardi.dev <mgagliardi.dev@gmail.com>
2024-06-22 12:04:15 +0200

    rm useless import

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:store_app/providers/logic_provider.dart';
 
